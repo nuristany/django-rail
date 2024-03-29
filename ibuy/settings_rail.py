d2d78c56863669ede65c238fb085bd33bb753720
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-03-30 02:26:07 +0430

    some changes

--- ibuy/settings_rail.py
+++ ibuy/settings_rail.py
@@ -5,5 +5,7 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = ['web-production-c403d.up.railway.app']  
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c403d.up.railway.app']
+
 #ALLOWED_HOSTS = ['127.0.0.1']  
 
