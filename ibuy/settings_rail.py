4d3615ff520fbb5f46f3cdc9d7e86e591d5dc0a3
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-03-30 02:45:40 +0430

    postgres db settings added

--- ibuy/settings_rail.py
+++ ibuy/settings_rail.py
@@ -7,5 +7,20 @@ SECRET_KEY = config('SECRET_KEY')
 ALLOWED_HOSTS = ['web-production-c403d.up.railway.app']  
 CSRF_TRUSTED_ORIGINS = ['https://web-production-c403d.up.railway.app']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+        'OPTIONS': {
+            'options': '-c search_path=public,store,core'
+        }
+
+    }
+}
+
 #ALLOWED_HOSTS = ['127.0.0.1']  
 
