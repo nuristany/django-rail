06a0541a8bafa2a250cadeba73e12e670ad52029
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-03-30 13:15:00 +0430

    sqlite3 db selected

--- ibuy/settings_rail.py
+++ ibuy/settings_rail.py
@@ -7,20 +7,20 @@ SECRET_KEY = config('SECRET_KEY')
 ALLOWED_HOSTS = ['web-production-036f.up.railway.app']  
 CSRF_TRUSTED_ORIGINS = ['https://web-production-036f.up.railway.app']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
-        'OPTIONS': {'sslmode': 'require'},
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT'),
+#         'OPTIONS': {'sslmode': 'require'},
             
         
 
-    }
-}
+#     }
+# }
 
 
 
