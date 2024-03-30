572641c7d3eae8ab8ff883c79ee8a2751b21d5ab
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-03-30 12:03:14 +0430

    Database settings updated

--- ibuy/settings_rail.py
+++ ibuy/settings_rail.py
@@ -7,6 +7,22 @@ SECRET_KEY = config('SECRET_KEY')
 ALLOWED_HOSTS = ['web-production-c403d.up.railway.app']  
 CSRF_TRUSTED_ORIGINS = ['https://web-production-c403d.up.railway.app']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT'),
+#         'OPTIONS': {'sslmode': 'require'},
+            
+        
+
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -15,6 +31,8 @@ DATABASES = {
         'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': config('DATABASE_HOST'),
         'PORT': config('DATABASE_PORT'),
+        'DATABASE_URL': config('DATABASE_URL'),
+        'DATABASE_PRIVATE_URL': config('DATABASE_PRIVATE_URL'),
         'OPTIONS': {'sslmode': 'require'},
             
         
