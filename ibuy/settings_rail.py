2bf1cedb5df31c6f9af4565bfcdc34b1bfb41b10
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-03-30 12:21:56 +0430

    made some changes in db settings

--- ibuy/settings_rail.py
+++ ibuy/settings_rail.py
@@ -23,22 +23,6 @@ CSRF_TRUSTED_ORIGINS = ['https://web-production-c403d.up.railway.app']
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
-        'DATABASE_URL': config('DATABASE_URL'),
-        'DATABASE_PRIVATE_URL': config('DATABASE_PRIVATE_URL'),
-        'OPTIONS': {'sslmode': 'require'},
-            
-        
-
-    }
-}
 
 #ALLOWED_HOSTS = ['127.0.0.1']  
 
