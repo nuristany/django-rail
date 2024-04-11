9bf675ff0c98a949a0e6b5660dd9b9b37bf71e1f
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-04-11 22:09:55 +0430

    made some changes settings and views

--- ibuy/settings.py
+++ ibuy/settings.py
@@ -158,9 +158,9 @@ REST_FRAMEWORK = {
        
     ),
 
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.AllowAny',
-    # ]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
 
     
 }
