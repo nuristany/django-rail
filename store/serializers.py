8ef5244155ec49f1c541794ae99f0be14249a75e
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-04-15 22:40:28 +0430

    modified  store/serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -30,35 +30,39 @@ class ItemImageSerializer(serializers.ModelSerializer):
 
 class ItemSerializer(serializers.ModelSerializer):
     images = ItemImageSerializer(many=True, read_only=True)
-    category = serializers.StringRelatedField()  # Allow category name to be submitted as a string
+    category_name = serializers.CharField(write_only=True)  # Make category_name writable
     phone = serializers.SerializerMethodField()
     seller_full_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Item
-        fields = ['id', 'title', 'price', 'description', 'seller', 'seller_full_name', 'phone', 'condition', 'category', 'images']
+        fields = ['id', 'title', 'price', 'description', 'seller', 'seller_full_name', 'phone', 'condition', 'category_name', 'images']
+
+    # Define get_phone and get_seller_full_name methods as before
 
     def get_phone(self, obj):
+        # Access the contact number from the associated UserProfile
         if obj.seller:
             return obj.seller.phone
         return None
     
     def get_seller_full_name(self, obj):
+        # Combine first_name and last_name into a single field
         if obj.seller:
             if obj.seller.first_name and obj.seller.last_name:
                 return f"{obj.seller.first_name} {obj.seller.last_name}"
             elif obj.seller.is_superuser:
-                return obj.seller.username
+                return obj.seller.username  # Return username if no first_name and last_name are provided for superuser
         return None
 
     def create(self, validated_data):
-        category_name = validated_data.pop('category')
+        category_name = validated_data.pop('category_name')
         category_instance, _ = Category.objects.get_or_create(name=category_name)
         validated_data['category'] = category_instance
         return Item.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        category_name = validated_data.pop('category', None)
+        category_name = validated_data.pop('category_name', None)
         if category_name:
             category_instance, _ = Category.objects.get_or_create(name=category_name)
             instance.category = category_instance
