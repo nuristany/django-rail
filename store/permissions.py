589bc6b1112156393b62f301902cf58bb238baf0
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-04-15 15:51:39 +0430

    modified the permissions

--- store/permissions.py
+++ store/permissions.py
@@ -1,22 +1,21 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
 
-class IsOwnerOrReadOnly(BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
 
-    def has_permission(self, request, view):
-        # Allow GET, HEAD or OPTIONS requests (read-only permissions)
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
-        
-        # Only authenticated users can modify objects
-        return request.user and request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj):
-        # Allow read-only permissions for non-authenticated users
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
             return True
-        
-        # Only the owner of the object can modify it
-        return obj.seller == request.user
+
+        # Write permissions are only allowed to the owner of the item.
+        if obj.seller == request.user:
+            return True
+        else:
+            # If the requesting user is not the owner, return a custom response
+            return Response("You do not have permission to perform this action.", status=status.HTTP_403_FORBIDDEN)
